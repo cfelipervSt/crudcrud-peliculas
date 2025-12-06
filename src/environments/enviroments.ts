d17c0729c50b843b332dd6bb899e24f06348982b
cfelipervSt <cfeliperv@gmail.com>
2025-12-06 13:18:44 -0500

    Proyecto peliculas v1

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://crudcrud.com/api/6ee5c7fd62fd47e7ac3fee20123f87fa'
+  apiBaseUrl: 'https://crudcrud.com/api/359382a589834548b4bfe0f5736833f2'
 };
